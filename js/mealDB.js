1dd2fe45a6f9c2d92d0062deceee78d154c79f83
Eshad Dewan <eshaddewan571@gmail.com>
2021-08-28 11:16:26 +0600

    almost

--- js/mealDB.js
+++ js/mealDB.js
@@ -19,14 +19,17 @@ const loadFoods = () => {
 
 const displayFood = meals => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    const details = document.getElementById('meal-detail');
+    details.textContent = '';
 
     meals.forEach(meal => {
         //each meal is an object
         const div = document.createElement('div');
-
+        // console.log(meal)
         div.classList.add('col');
         div.innerHTML = ` 
-        <div class="card">
+        <div onclick="mealDetails(${meal.idMeal})" class="card shadow">
            <img height="250" width
            "250" src="${meal.strMealThumb}" class="card-img-top p-3" alt="...">
            <div class="card-body">
@@ -37,6 +40,39 @@ const displayFood = meals => {
         
         `
         searchResult.appendChild(div);
-    })
+    });
 
+}
+
+const mealDetails = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals))
+
+    //the data gives us an object.and the meals oproperty of the object has an array. the array has only one element . which is an object and the meal information
+}
+
+const displayMealDetails = meal => {
+    //since the meal array has only one item, we can save the info in variable
+    const mealDetail = meal[0];
+    const details = document.getElementById('meal-detail');
+    details.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.classList.add('mb-3');
+
+
+    div.innerHTML = `
+                <img width="500" height="500" src="${mealDetail.strMealThumb}" class="card-img-top p-2" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${mealDetail.strMeal}</h5>
+                    <p class="card-text">${mealDetail.strInstructions.slice(0, 500)}</p>
+                    <a class="text-decoration-none" href="${mealDetail.strYoutube}">See on Youtube</a>
+                    <a class=" text-decoration-none"> </a>
+                </div>
+    `
+
+    details.appendChild(div);
 }
